cce3860935002a0de7cc08a560681da8b56187eb
KevinWu <kevin84020202@gmail.com>
2017-12-28 12:29:51 +0800

    Fix bug

--- craw_day.py
+++ craw_day.py
@@ -1,11 +1,58 @@
 import day_data
+import json
 
-# y = 2017
-for y in range(2008, 2018):
-    # 跑所有個月份
-    for m in range(1, 13):
+
+# for y in range(2016, 2017):
+#     # 跑所有個月份
+#     for m in range(1, 13):
+#         if m < 10:
+#             m = "0" + str(m)
+#             # print(m)
+#         day_data.get_data(y, m)
+#         print(y, "/", m)
+
+
+for y in range(2008, 2009):
+    # 將每年一月的json檔讀取當作基準
+    path = "data3/" + str(y) + "-01" + ".json"
+    json_data = open(path).read()
+    data = json.loads(json_data)
+    # print(data["ObsTime"])
+
+    # 依序將當年其他月份資料合併
+    for m in range(2, 13):
+        # 可改寫法
         if m < 10:
             m = "0" + str(m)
-            day_data.get_data(y, m)
-        day_data.get_data(y, m)
-        print(m)
+        path1 = "data3/" + str(y) + "-" + str(m) + ".json"
+
+        json_data1 = open(path1).read()
+
+        # 讀成dict
+        data1 = json.loads(json_data1)
+        # print(data["ObsTime"])
+        # print(path1)
+        # 找到相同key的欄位合併資料
+        for key, value in data.items():
+            # 合併list
+
+            data[key].extend(data1[key])
+
+
+print(len(data["Precp"]))
+
+# 把數值為T的去掉
+count = 0
+for p in data["Precp"]:
+    if p == "T":
+        data["Precp"][count] = 0.1
+        # print(p)
+    count = count + 1
+
+
+
+# # 合併完所有年份資料後另存新檔
+# addr = "data3/" + "target" + ".json"
+# f = open(addr, 'w')
+# f.write(json.dumps(data, indent=4, ensure_ascii=False))
+
--- day_data.py
+++ day_data.py
@@ -7,7 +7,7 @@ import os
 
 # 抓降雨因素資料
 def get_data(year, month):
-    url = 'http://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station=466900&stname=%25E6%25B7%25A1%25E6%25B0%25B4&datepicker=2017-'
+    url = 'http://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station=466900&stname=%25E6%25B7%25A1%25E6%25B0%25B4&datepicker='
 
     # 要讀取的日期
 
@@ -92,37 +92,3 @@ def get_data(year, month):
     f.write(json.dumps(weather, indent=4))
 
 
-def merge():
-
-# 將起始日期json檔讀取當作基準
-path = "data2/" + str(year) + ".json"
-json_data = open(path).read()
-data = json.loads(json_data)
-
-# 讀取之前年份資料，依序合併其他年份資料
-for j in range((year - 1), (year - num), -1):
-    path = "data2/" + str(j) + ".json"
-    json_data = open(path).read()
-
-    # 讀成dict
-    data1 = json.loads(json_data)
-    # print(len(data.values()))
-
-    # 找到相同key的欄位合併資料
-    for key, value in data.items():
-        # 合併list
-        data[key].extend(data1[key])
-
-count = 0
-# 把數值為T的去掉
-for p in data["Precp"]:
-    if p == "T":
-        data["Precp"][count] = 0.1
-        # print(p)
-    count = count + 1
-# print(data)
-
-# 合併完所有年份資料後另存新檔
-addr = "data2/" + str(year) + "(" + str(num) + ")" + ".json"
-f = open(addr, 'w')
-f.write(json.dumps(data, indent=4, ensure_ascii=False))
